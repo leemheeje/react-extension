1d454bb421e48f5a37893bca1037dc4d23fa5d08
leemheeje <dlagmleo89@gmail.com>
2021-05-20 21:09:47 +0900

    파일정리

--- src/App.js
+++ src/App.js
@@ -1,28 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js 이므 임희잭다 변경했다!! 마스터 안겨쳤다!
-            
-            asdfasdfasdf되냐되냐asdfasdfasdf되냐되냐asdfasdfasdf되냐되냐
-            
-            
-            히재임121231233121212희재</code> 
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     1
     </div>
   );
 }
