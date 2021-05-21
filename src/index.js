e41b9043bc5fdc59fe2769a8a447bcacdfa4f457
leemheeje <dlagmleo89@gmail.com>
2021-05-21 17:49:11 +0900

    파이어베이스 추가셋팅

--- src/index.js
+++ src/index.js
@@ -5,15 +5,19 @@ import firebaseConfig from './firebaseConfig';
 import "firebase/auth";
 import "firebase/firestore";
 import App from './App';
+import testsetset from './testsetset';
 
-firebase.initializeApp(firebaseConfig);
-firebase.auth().createUserWithEmailAndPassword('dlagm123213123leo89@gmail.com', 'asdfasdfasdf1121212').then(res=>{
-  console.log(res);
-})
-var provider = new firebase.auth.GoogleAuthProvider();
-firebase.auth().signInWithPopup(provider).then(res=>{
-  console.log(res);
-})
+console.log(testsetset);
+
+
+// firebase.initializeApp(firebaseConfig);
+// firebase.auth().createUserWithEmailAndPassword('dlagm123213123leo89@gmail.com', 'asdfasdfasdf1121212').then(res=>{
+//   console.log(res);
+// })
+// var provider = new firebase.auth.GoogleAuthProvider();
+// firebase.auth().signInWithPopup(provider).then(res=>{
+//   console.log(res);
+// })
 ReactDOM.render(
   <React.StrictMode>
     <App />
---
+++ src/testsetset.js
@@ -0,0 +1,7 @@
+import React from 'react';
+const testsetset = ()=>{
+    return (
+        <div className="asdf">12</div>
+    );
+}
+export default testsetset;
