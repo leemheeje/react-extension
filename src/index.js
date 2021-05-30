3545e3eeaedf16bd3e5619f8e50b4de2f4ce1cc6
leemheeje <dlagmleo89@gmail.com>
2021-05-31 08:49:34 +0900

    log

--- src/index.js
+++ src/index.js
@@ -1,27 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import firebase from "firebase/app";
+import firebase from 'firebase';
 import firebaseConfig from './firebaseConfig';
-import "firebase/auth";
-import "firebase/firestore";
 import App from './App';
-import testsetset from './testsetset';
 
-console.log(testsetset);
-
-
-// firebase.initializeApp(firebaseConfig);
-// firebase.auth().createUserWithEmailAndPassword('dlagm123213123leo89@gmail.com', 'asdfasdfasdf1121212').then(res=>{
-//   console.log(res);
-// })
-// var provider = new firebase.auth.GoogleAuthProvider();
-// firebase.auth().signInWithPopup(provider).then(res=>{
-//   console.log(res);
-// })
+firebase.initializeApp(firebaseConfig);
+//logs
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<App /> 
+	</React.StrictMode>,
+	document.getElementById('root')
 );
-
--- src/testsetset.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-const testsetset = ()=>{
-    return (
-        <div className="asdf">12</div>
-    );
-}
-export default testsetset;
